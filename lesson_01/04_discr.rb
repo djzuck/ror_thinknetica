5ef74417add1064931031d9d6db9be1045b90917
Stas Baleskov <djzuck@gmail.com>
2018-11-30 22:57:25 +0300

    made changes in 02 and 04

--- lesson_01/04_discr.rb
+++ lesson_01/04_discr.rb
@@ -7,15 +7,15 @@ b = gets.chomp.to_i
 print "Введи коэффициент c: "
 c = gets.chomp.to_i
 
-D = b**2 - 4 * a * c
+d = b**2 - 4 * a * c
 
-if D < 0
+if d < 0
   puts "Корней нет."
-elsif D == 0
-  puts "Дискриминант: #{ D }, корень: #{ x = -b/2 * a }."
-else D > 0
-  m = Math.sqrt(D)
-  x1 = (-b + m )/2 * a
-  x2 = (-b - m )/2 * a
-  puts "Дискриминант: #{ D }, корень 1: #{ x1 }, корень 2 #{ x2 }."
+elsif d == 0
+  puts "Дискриминант: #{ d }, корень: #{ x = -b/2 * a }."
+else d > 0
+  m = Math.sqrt(d)
+  x1 = (-b + m) / 2 * a
+  x2 = (-b - m) / 2 * a
+  puts "Дискриминант: #{ d }, корень 1: #{ x1 }, корень 2 #{ x2 }."
 end
