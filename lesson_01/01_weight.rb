a92ff53a7be29ebdc0337e7ad4e7900bae29412f
Stas Baleskov <djzuck@gmail.com>
2018-11-30 19:27:34 +0300

    made changes in data conversion in 01_weight

--- lesson_01/01_weight.rb
+++ lesson_01/01_weight.rb
@@ -2,9 +2,9 @@ print "Hi. What's your name? "
 name = gets.chomp
 
 print "Enter your height, please: "
-height = gets.chomp
+height = gets.chomp.to_f
 
-weight = height.to_f - 110
+weight = height - 110
 
 if weight > 0
   puts "#{ name }, your perfect weight #{ weight }."
